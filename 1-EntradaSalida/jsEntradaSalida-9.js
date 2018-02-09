097c37517348fc12f7a264a6d607b74ecdac76af
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-09 20:08:23 -0300

    Ejercicios 8 y 9
    Resto, porcentajes, regla de tres simple

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,20 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var resultado;
+
+	sueldo=document.getElementById('sueldo').value;
+
+	sueldo=parseInt(sueldo);
+
+	resultado= sueldo + (sueldo*0.10);
+
+	/*resultado= sueldo * 1.10 (es el 100% -sueldo- + 10% = 110% = 1.1)
+
+	resultado=parseInt(resultado);*/
+
+	document.getElementById('resultado').value=resultado;
 
 	
 }
