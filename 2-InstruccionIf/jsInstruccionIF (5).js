79abe914bb9b38120587c0525454d7686ce7e35b
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-09 22:14:07 -0300

    Ejercicio 5
    if: ||

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -6,7 +6,7 @@ var edad
 edad=document.getElementById('edad').value;
 edad=parseInt(edad);
 
-if (edad < 13 || edad > 17) // || porque tiene que ser uno u otro ningún nro cumple los dos &&
+if (edad < 13 || edad > 17) // || porque tiene que ser uno u otro ningún nro. cumple los dos &&
 {
 	alert("No es adolescente");
 }
