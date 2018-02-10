7fabc724a62a2dc27f84ad3fbe810746c6c0b7e9
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-09 22:05:48 -0300

    Ejercicio 4
    if: un if dentro de otro y  rango

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+
+edad=document.getElementById('edad').value;
+edad=parseInt(edad)
+
+if (edad <= 17 && edad >= 13)
+{
+	alert("Es adolescente");
+}
+
+/*
+if (edad >= 13)
+{
+	if (edad <=17 )
+  {
+	alert("Es adolescente");
+  }
+}*/
 
 
 
