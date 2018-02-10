ece5c64819bd6134db9173240c94c05bb71893cc
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-09 22:11:21 -0300

    Ejercicio 5
    if: rango y ||

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
 
+edad=document.getElementById('edad').value;
+edad=parseInt(edad);
+
+if (edad < 13 || edad > 17) // || porque tiene que ser uno u otro ningún nro cumple los dos &&
+{
+	alert("No es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
