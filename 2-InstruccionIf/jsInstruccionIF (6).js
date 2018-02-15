6a4d8309fb363ce2c34f6b99a490bbc408216a8e
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-15 17:53:35 -0300

    Ejercicio 6
    else if

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,24 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad=document.getElementById('edad').value;
+
+edad=parseInt(edad);
+
+if (edad >= 18)
+{
+	alert("Es mayor de edad");
+}
+else if (edad < 18 && edad >= 13)
+{
+	alert("Es adolescente");
+}
+else
+{
+	alert("Es un niño");
+}
 
 
 }//FIN DE LA FUNCIÓN
