d5f56884760d88d9057a1a944629010abf2ed792
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-15 21:52:38 -0300

    Recuperatorio ejercicio 2
    Prompt, resta

--- 9-Parciales/recuperatorio/dos.js
+++ 9-Parciales/recuperatorio/dos.js
@@ -1,4 +1,12 @@
 function Mostrar()
 {
-  
+  var importePrompt;
+  var importeFinal;
+
+  importePrompt=prompt("Ingrese el importe");
+  importePrompt=parseInt(importePrompt);
+
+  importeFinal=importePrompt - (importePrompt*0.25);
+
+  document.getElementById('importeFinal').value=importeFinal;
 }
