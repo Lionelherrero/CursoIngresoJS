78f21a763875a50a9f4863bbd9706ed27e6e5905
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-15 21:11:44 -0300

    Ejercicios 7, 8, 9 y 10
    if, elese, Math.random()

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var notaDelExamen;
+
+	notaDelExamen=Math.floor((Math.random()*10)+1);
+
+	if (notaDelExamen < 4)
+	{
+		alert("Vamos, la próxima se puede");
+	}
+	else
+	{
+		if (notaDelExamen < 9)
+		{
+			alert("APROBÓ");
+		}
+		else
+		{
+			alert("EXCELENTE");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
