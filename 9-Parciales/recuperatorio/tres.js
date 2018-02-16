db7ac28d6aab77ff1dcc1ab7fa0f95da342c3100
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-15 22:02:39 -0300

    Recuperatorio ejercicio 3
    3 Id, promedio

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var promedio;
 
+	precioUno=document.getElementById('precioUno').value;
+	precioDos=document.getElementById('precioDos').value;
+	precioTres=document.getElementById('precioTres').value;
+
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+
+	promedio= (precioUno + precioDos + precioTres)/3
+
+	alert("El promedio es: "+promedio);
 }
