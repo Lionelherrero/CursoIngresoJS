d10cc9de0b60d63ad80546dae3256759448db79f
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-15 21:43:00 -0300

    Recuperatorio uno
    Operaciones matemáticas

--- 9-Parciales/recuperatorio/uno.js
+++ 9-Parciales/recuperatorio/uno.js
@@ -1,5 +1,13 @@
 
 function Mostrar()
 {
+	var base;
+	var superficie;
 
+	base=document.getElementById('laBase').value;
+	base=parseInt(base)
+
+	superficie=base*base;
+
+	alert("La superficie del cuadrado es: "+superficie);
 }
