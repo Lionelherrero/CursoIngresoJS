739593f20564ae134b612d5af2d85f801c03ea87
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-16 21:47:32 -0300

    Ejercicio 1, 2, 3 y 4
    Switch, case, default

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,30 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+
+	switch (mesDelAño)
+	{
+		case "Enero": //Tiene que ser igual al "option value" del html!!
+					alert("que comiences bien el año!!!");
+					break;
+
+		case "Marzo":
+					alert("a clases!!!");
+					break;
+
+		case "Julio":
+					alert("se vienen las vacaciones!!!");
+					break;
+
+		case "Diciembre":
+						alert("Felices fiestas!!!");
+						break;
+
+		// No se pone default porque hay otras opciones
+	}
+
+
 
 
 
