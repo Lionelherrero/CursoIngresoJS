e5e226e4053941c46cba531f5671f8ea44d084c6
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-23 19:28:02 -0300

    Ejercicio 7
    while, validar un número (isNaN()), confirm

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -6,16 +6,56 @@ function Mostrar()
 	var respuesta='si';
 	var numero;
 
-	while(respuesta=="si") //depende lo que diga arriba
+	while(confirm("Si desea ingresar un número, presione aceptar"))
+	/*while(respuesta=="si") //depende lo que diga arriba*/
 	{
 		contador=contador+1;
 
 		numero=prompt("ingrese un número");
 		numero=parseInt(numero);
+		
+		//validar si es un numero:
+		while (isNaN(numero))
+		{
+			numero=prompt("ingrese un número");
+			numero=parseInt(numero);
+		}
+
+		//Si se toca cancelar, devuelve null
+		/*
+		opcion 2: 
+			if (numero==null)
+			{
+				respuesta="no";
+				continue // corta la ejecución y vuelve a
+						 // la condición
+			}
+
+		contador++;
+		
+		opción 3:
+
+		while (confirm()) // Devuelve true o false
+
+		opción 4: (no sirve si se equivoca el usuario)
+
+		parseInt() devuleve NaN si no se ingresa número
+
+		while (isNan (numero)) devuleve true o false
+
+		validar que es un numero
+
+		while (isNan (numero))
+		{
+			numero=prompt();
+			numero=parseint(numero);
+		}
+
+		*/
 
 		acumulador= acumulador+numero;
 
-		respuesta= prompt("Para continuar, si");
+		//respuesta= prompt("Para continuar, si");
 	}
 
 
