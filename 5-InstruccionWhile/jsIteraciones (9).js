3782ccce7a41926200dc4a2db4037db61a20aab3
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-23 19:33:30 -0300

    Ejercicio 9
    while, número máximo y mínimo

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -4,13 +4,48 @@ function Mostrar()
 	var contador=0;
 	// declarar variables
 	
+	var numero=0;
+	var maximo;
+	var minimo;
+
+	
 	var respuesta='si';
 
-	while(respuesta!='no')
+	while (respuesta!='no')
 	{
+		numero=prompt("Ingrese un número:");
+		numero=parseInt(numero);
+
+		contador++;
+
+		if (contador == 1)
+		{
+			maximo=numero;
+			minimo=numero;
+		}
+		else
+		{
+			//minimo=numero;
 		
+
+			if (numero > maximo)
+			{
+				maximo=numero;
+			}
+			else
+			{
+				if (numero < minimo)
+				{
+					minimo=numero;
+				}
+			}
+		}
+		respuesta=prompt("Si desea salir escriba no");	
 	
 	}
+
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 
 
 
