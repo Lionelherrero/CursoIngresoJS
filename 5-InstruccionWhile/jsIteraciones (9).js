d15449c328d47af4487214b170bedfef07377fe9
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-02-23 20:01:52 -0300

    Ejercicio 9
    while, maximos y minimos

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -5,8 +5,8 @@ function Mostrar()
 	// declarar variables
 	
 	var numero=0;
-	var maximo;
-	var minimo;
+	var maximo; //no le podemos dar valor
+	var minimo; //no le podemos dar valor
 
 	
 	var respuesta='si';
@@ -16,10 +16,21 @@ function Mostrar()
 		numero=prompt("Ingrese un número:");
 		numero=parseInt(numero);
 
-		contador++;
-
-		if (contador == 1)
+		while (isNaN(numero))
 		{
+			numero=prompt("ingrese un número");
+			numero=parseInt(numero);
+		}
+
+		// Cuando no sabemos el max. o mín. Usamos el contador de bandera
+		// para saber cuando es la primera 1ª vez
+		// que ingresa el máximo=mínimo
+
+		// Si fuese de 1 a 10
+		//Max=0 y Min=11;
+		if (contador == 0)
+		{
+			contador++;
 			maximo=numero;
 			minimo=numero;
 		}
