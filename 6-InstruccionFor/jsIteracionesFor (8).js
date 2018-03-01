a01f1b1938d783c14b4f59d5a16051201840da35
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-03-01 17:39:34 -0300

    Ejercicio 8
    For, números primos

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,31 @@
 function Mostrar()
 {
+	var numeroIngresado;
 
+	numeroIngresado=prompt("Ingrese un numero para saber si es primo o no:");
+	numeroIngresado=parseInt(numeroIngresado);
+
+	for (i=2; i <= numeroIngresado; i++)
+	{
+		if (numeroIngresado%i == 0)
+		{
+			if (numeroIngresado/i == 1 && numeroIngresado/1 == numeroIngresado)
+			{
+				alert("Es un número primo.");
+			}
+			else
+			{
+				alert("No es un número primo.");
+				break;
+			}	
+		}
+		
+	}
+
+	if (numeroIngresado == 0 || numeroIngresado == 1)
+	{
+		alert("No es un número primo.");
+	}
 
 
 
