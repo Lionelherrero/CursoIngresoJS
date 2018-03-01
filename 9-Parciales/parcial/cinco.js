f855029f6de0517e54b2dc17e7205e41976e0826
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-03-01 18:55:11 -0300

    Ejercicio 5 Parcial
    Switch

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,20 @@
 function Mostrar()
 {
+	var dia;
+
+	dia=prompt("Ingrese un día:");
+
+	switch (dia)
+	{
+		case "Sábado":
+		case "Domingo":
+					alert("Es fin de semana.");
+					break;
+		default:
+					alert("¡¡¡A trabajar!!!");
+					break;
+	}
+
+
 
 }
