f96285041c89eec441710b8ead79e07ee76930ba
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-03-02 19:16:51 -0300

    Ejercicio 7

--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -3,8 +3,20 @@ function Mostrar()
 	var nota;
 	var sexo;
 
+	var totalNotas=0;
+
+	var promedioNotas;
+	var cantidadAlumnos=0;
+
+	var notaMasBaja;
+
+	var cantidadDeVaronesNotaSeis=0;
+
 	for (i=1; i <= 100; i++)
 	{
+		
+		cantidadAlumnos++;
+
 		nota=prompt("Ingrese la nota del alumno:");
 		nota=parseInt(nota);
 
@@ -22,7 +34,34 @@ function Mostrar()
 			alert("Error. Ingrese f o m.");
 			sexo=prompt("Ingrese 'f' para femenino o 'm' para masculino:");
 		}
+
+		totalNotas=totalNotas+nota;
+
+		if (i==1)
+		{
+			notaMasBaja=nota;
+		}
+		else
+		{
+			if (nota < notaMasBaja)
+			{
+				notaMasBaja=nota;
+			}
+		}
+
+		if (sexo== "m" && nota >= 6)
+		{
+			cantidadDeVaronesNotaSeis++;
+		}
+
 	}
+
+	promedioNotas=totalNotas/cantidadAlumnos;
+	alert("El promedio de las notas es: "+promedioNotas);
+
+	alert("La nota más baja es: "+notaMasBaja);
+
+	alert("La cantidad de varones con nota seis o más es: "+cantidadDeVaronesNotaSeis);
 
 	
 
