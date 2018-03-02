0521fc87de42145f544c5bd069ccd524a440caa4
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-03-02 19:45:39 -0300

    Ejercicio 8

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,72 @@
 function Mostrar()
 {
+	var numeroPositivo;
+	var respuesta="si";
+
+	var contadorNumerosPares=0;
+
+	var acumuladorNumerosPositivos=0;
+	var contadorNumerosPositivos=0;
+	var promedioNumerosPositivos=0;
+
+	var numeroMaximo;
+	var numeroMinimo;
+
+
+	while (respuesta == "si")
+	{
+		numeroPositivo=prompt("Ingrese un numero positivo:");
+		numeroPositivo=parseInt(numeroPositivo);
+
+		while(isNaN(numeroPositivo) || numeroPositivo <= 0 )
+		{
+			alert("Error. Ingrese un número positivo.");
+			numeroPositivo=prompt("Ingrese un numero positivo:");
+			numeroPositivo=parseInt(numeroPositivo)
+		}
+
+		contadorNumerosPositivos++;
+		acumuladorNumerosPositivos=acumuladorNumerosPositivos + numeroPositivo;
+
+		if (numeroPositivo%2 == 0 /*&& numeroPositivo != 0*/)
+		{
+			contadorNumerosPares++;
+		}
+
+		if (contadorNumerosPositivos==1)
+		{
+			numeroMinimo=numeroPositivo;
+			numeroMaximo=numeroPositivo;
+		}
+		else
+		{
+			if (numeroPositivo > numeroMaximo)
+			{
+				numeroMaximo=numeroPositivo;
+			}
+			else
+			{
+				if (numeroPositivo < numeroMinimo)
+				{
+					numeroMinimo=numeroPositivo;
+				}
+			}
+		}
+
+
+
+		respuesta=prompt("Si desea continuar escriba 'si'.");
+	}
+
+	
+	document.write("La cantidad de números pares es: "+contadorNumerosPares);
+
+	promedioNumerosPositivos=acumuladorNumerosPositivos/contadorNumerosPositivos;
+	document.write("<br>El promedio de los números positivos es: "+promedioNumerosPositivos)
+
+	document.write("<br>La suma de todos los números es: "+acumuladorNumerosPositivos)
+
+	document.write("<br>El número máximo es: "+numeroMaximo);
+	document.write("<br>El número mínimo es: "+numeroMinimo);
 
 }
